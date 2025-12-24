512116280fd2cee461ec842ed0e202437c0f1797
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-12-24 16:34:04 -0500

    Add routes for Adaptive Heist and Donut 3D Blender Art pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { AherosjourneyComponent } from './game-dev/a-heros-journey/a-heros-journey.component';
 import { AverysadventureComponent } from './game-dev/averys-adventure/averys-adventure.component';
 import { BallrollingComponent } from './game-dev/ball-rolling/ball-rolling.component';
+import { Donut3dBlenderArtComponent } from './game-dev/donut-3d-blender-art/donut-3d-blender-art.component';
+import { AdaptiveheistComponent } from './game-dev/adaptive-heist/adaptive-heist.component';
 import { ProjectsResolver } from './resolvers/projects.resolver';
 import { StartComponent } from './start/start.component';
 
@@ -15,6 +17,8 @@ export const routes: Routes = [
   { path: 'aherosjourney', component: AherosjourneyComponent },
   { path: 'ballrolling', component: BallrollingComponent },
   { path: 'averysadventure', component: AverysadventureComponent },
+  { path: 'donut3dblenderart', component: Donut3dBlenderArtComponent },
+  { path: 'adaptiveheist', component: AdaptiveheistComponent },
   { path: '', component: StartComponent },
   { path: '**', component: HomeComponent },
 ];
